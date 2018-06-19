798da15d6819b7bb915fd67c95545f1249c64009
slapa97 <34189510+slapa97@users.noreply.github.com>
2018-06-19 22:21:49 +0200

    Update MainActivity.java
    test

--- app/src/main/java/com/example/PK_Java_Project/MainActivity.java
+++ app/src/main/java/com/example/PK_Java_Project/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+//ss
 
 import java.util.ArrayList;
 import java.util.Collections;
