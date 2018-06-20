b63839ef9a88a33dffbc5895773ff9ff0293f02a
maly-slapa <slapinski.p@gmail.com>
2018-06-20 15:42:05 +0200

    layout update - new edit texts

--- app/src/main/java/com/example/PK_Java_Project/OneProduct.java
+++ app/src/main/java/com/example/PK_Java_Project/OneProduct.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
 
 
 public class OneProduct extends AppCompatActivity {
@@ -32,6 +32,21 @@ public class OneProduct extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (spinner.getSelectedItem().toString().equals("Clothes")) {
+                    country_edit.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                country_edit.setVisibility(View.INVISIBLE);
+            }
+        });
 
 
         button_one.setOnClickListener(new View.OnClickListener() {
